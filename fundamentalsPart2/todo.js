56505de97406f137112cacc85f63623ab151da71
Amanda Fry <feamandaf@hotmail.com>
2016-08-02 20:49:51 -0700

    added 2nd assigment complete

--- fundamentalsPart2/todo.js
+++ fundamentalsPart2/todo.js
@@ -4,12 +4,16 @@
 
 // Normal function definition:
 console.log(" - normal function")
+
 function hello() {
   console.log('Hello world');
 }
 hello();
+
+
 //Anonymous function definition:
 console.log(" - Anonymous function")
+
 var anon = function() {
   console.log('I am anonymous');
 };
@@ -18,42 +22,143 @@ anon();
 console.log('1st problem')
 console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
 
-var mySum = 0;
-for(var i = 1; i<=500; i++){
-	mySum = mySum + i;
-}
-console.log(mySum);
-
-console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
-
-
-console.log('2nd')
-
-//Write a loop that will go through the array [1, 5, 90, 25, -3, 0], find the minimum value, and then print it
-var myArray = [1, 5, 90, 25, -3, 0];
-var min = myArray[0];
-for (var i = 1; i < myArray.length; i++){
-	if (myArray[i] < min){
-		min = myArray[i] ;
+var anonSum = function(){
+	var mySum = 0;
+	for(var i = 1; i<=500; i++){
+		mySum = mySum + i;
 	}
+	console.log('anonymous function, sum of numbers', mySum);
 }
-console.log(min);
+anonSum();
+console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+console.log('2nd problem')
+console.log('###########################################');
+
+var anonMin = function(){
+	//Write a loop that will go through the array [1, 5, 90, 25, -3, 0], find the minimum value, and then print it
+	var myArray = [1, 5, 90, 25, -3, 0];
+	var min = myArray[0];
+	for (var i = 1; i < myArray.length; i++){
+		if (myArray[i] < min){
+			min = myArray[i] ;
+		}
+	}
+	console.log(min);
+}
+anonMin();
 console.log('###########################################');
 console.log('3rd')
 
 
 //Write a loop that will go through the array [1, 5, 90, 25, -3, 0], find the average of all of the values, and then print it
-var avgArray = [1, 5, 90, 25, -3, 0];
-var avg = 0;
-var finalAvg;
+var anonAvg = function(){
+	var avgArray = [1, 5, 90, 25, -3, 0];
+	var avg = 0;
+	var finalAvg;
 
-for (var i = 1; i < avgArray.length; i++){
-	avg = avg + avgArray[i];
+	for (var i = 1; i < avgArray.length; i++){
+		avg = avg + avgArray[i];
+	}
+	finalAvg = avg/avgArray.length;
+	console.log(finalAvg);
 }
-finalAvg = avg/avgArray.length;
-console.log(finalAvg);
+anonAvg();
 console.log('...........................................');
 console.log('4th')
+console.log('solve as JSON object')
+
+//this need to be anonymus as the others. As an on object creating a JSON object.
+var person = {
+	//properties
+	name: 'Amanda',
+	distance_traveled: 0,
+	//methods
+	say_name: function(){console.log(this.name)},
+	say_something: function(saying){console.log(this.name + ' says ' + saying)},
+	walk: function(){console.log(this.name + ' is walking'); this.distance_traveled+=3;},
+	run: function(){console.log(this.name + ' is runing'); this.distance_traveled+=10;},
+	crawl: function(){console.log(this.name + ' is crawling'); this.distance_traveled+=1;},
+}
+//I can use say_name method to print the name property
+person.say_name();
+//I have to print the other porprt like this
+console.log(person.distance_traveled);
+person.say_something('lets walk');
+person.walk();
+console.log(person.distance_traveled);
+person.say_something('lets run');
+person.run();
+console.log(person.distance_traveled);
+person.say_something('lets crawl');
+person.crawl();
+console.log(person.distance_traveled);
+
 console.log('*******************************************');
 console.log('5th')
+console.log('solve as function')
+
+function woman(){
+	this.name = 'Amy';
+	this.distance_traveled = 0;
+	this.say_name = function(){console.log(this.name)};
+	this.say_something = function(saying){console.log(this.name + ' says ' + saying)};
+	this.walk = function(){console.log(this.name + ' is walking'); this.distance_traveled+=3;};
+	this.run = function(){console.log(this.name + ' is running'); this.distance_traveled+=10;};
+	this.crawl = function(){console.log(this.name + ' is crawling'); this.distance_traveled+=1;};
+}
+
+var Amy = new woman()
+Amy.say_name();
+console.log(Amy.distance_traveled);
+Amy.say_something("lets crawl");
+Amy.crawl();
+console.log(Amy.distance_traveled)
+Amy.say_something("lets walk");
+Amy.walk();
+console.log(Amy.distance_traveled);;
+Amy.say_something("lets run");
+Amy.run();
+console.log(Amy.distance_traveled);
+
 console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+
+/*
+school provided answer
+This is a basic person object, declared as an object literal, it has
+  properties: name, distanceTraveled,
+  methods: aayName, saySomething, walk, run, crawl
+  "DON'T FORGET THE COMMAS AFTER EACH METHOD/PROPERTY"
+*/
+
+var person = {
+  name : "Charlie",
+  distance_traveled : 0,
+  say_name : function(){
+    console.log(person.name);
+  },
+  say_something : function(phrase){
+    console.log(`${person.name} says: ${phrase}`);
+  },
+  walk : function(){
+    console.log(`${person.name} is walking!`);
+    person.distance_traveled += 3;
+    return person;
+  },
+  run : function(){
+    console.log(`${person.name} is running!`);
+    person.distance_traveled += 10;
+    return person;
+  },
+  crawl : function(){
+    console.log(`${person.name} is crawling!`);
+    person.distance_traveled += 1;
+    return person;
+  },
+  chewGum:function(){
+    console.log("I can walk and chew gum, but I can't chew gum and walk...");
+  }
+}
+
+person.walk().chewGum(); // why can I do this?
+// but not this:
+// person.chewgum().walk();
